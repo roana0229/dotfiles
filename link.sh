c8b8bcb86cfcf4a99a91ab18171bfec0b1678bfc
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-01-26 04:22:59 +0900

    Add link.sh

---
+++ link.sh
@@ -0,0 +1,6 @@
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+# ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
+# ln -sf ~/dotfiles/.git-completion.bash /usr/local/git/contrib/completion/.git-completion.bash
+
