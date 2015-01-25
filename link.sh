ed56f27d355f17c45c92ab5ad3b00339f59f854b
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-01-26 04:24:53 +0900

    Update

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-# ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
-# ln -sf ~/dotfiles/.git-completion.bash /usr/local/git/contrib/completion/.git-completion.bash
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+mkdir -p /usr/local/git/contrib/completion
+ln -sf ~/dotfiles/.git-completion.bash /usr/local/git/contrib/completion/.git-completion.bash
+ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
 
