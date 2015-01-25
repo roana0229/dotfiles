4c2ded9a8ee2618d8086a681620d601c8702414f
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-01-26 05:14:51 +0900

    Fix git-conpletion.bash, link.sh

--- .bashrc
+++ .bashrc
@@ -15,9 +15,9 @@ alias vibashp='vim ~/.bash_profile'
 alias rebash='source ~/.bashrc'
 alias rebashp='source ~/.bash_profile'
 
-if [ -r /usr/local/git/contrib/completion/.git-completion.bash ]; then
+if [ -r /usr/local/git/contrib/completion/git-completion.bash ]; then
   # terminal show branch name
-  # source /usr/local/git/contrib/completion/.git-completion.bash
+  # source /usr/local/git/contrib/completion/git-completion.bash
   GIT_PS1_SHOWDIRTYSTATE=true
   export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[33m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
 else
--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 ## Home Macbook
-#cat bashrc_home_mac.txt >> ~/dotfiles/.bashrc
-#cat bashprofile_home_mac.txt >> ~/dotfiles/.bash_profile
+# cat bashrc_home_mac.txt >> ~/dotfiles/.bashrc
+# cat bashprofile_home_mac.txt >> ~/dotfiles/.bash_profile
 
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 mkdir -p /usr/local/git/contrib/completion
-ln -sf ~/dotfiles/.git-completion.bash /usr/local/git/contrib/completion/.git-completion.bash
+ln -sf ~/dotfiles/git-completion.bash /usr/local/git/contrib/completion/git-completion.bash
 ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
