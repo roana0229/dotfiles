67d484def5bd1b77bd82dcddd649363f922aa0c7
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-01-26 04:33:48 +0900

    Add home macbook setting

--- link.sh
+++ link.sh
@@ -1,3 +1,7 @@
+## Home Macbook
+#cat bashrc_home_mac.txt >> ~/dotfiles/.bashrc
+#cat bashprofile_home_mac.txt >> ~/dotfiles/.bash_profile
+
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
@@ -5,4 +9,3 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 mkdir -p /usr/local/git/contrib/completion
 ln -sf ~/dotfiles/.git-completion.bash /usr/local/git/contrib/completion/.git-completion.bash
 ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
-
