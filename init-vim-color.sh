632fc2c3f317670d3304ef077eccf102bdd2549d
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-08-31 16:37:35 +0900

    Update excute init-vim-color.sh after remove backup wqfile

--- init-vim-color.sh
+++ init-vim-color.sh
@@ -1,7 +1,8 @@
 sh -c "$(curl -fsSL https://raw.github.com/Shougo/neobundle.vim/master/bin/install.sh)";
 git clone https://github.com/tomislav/osx-terminal.app-colors-solarized ~/osx-terminal.app-colors-solarized
 LF=$'\\\x0A'
-sed -i -e 's/\" for replase: vim color setting/syntax enable'"$LF"'set background=dark'"$LF"'colorscheme solarized'"$LF"'let g:solarized_termcolors=256/' .vimrc
+sed -i -e 's/\" for replase: vim color setting/syntax enable'"$LF"'set background=dark'"$LF"'colorscheme solarized'"$LF"'let g:solarized_termcolors=256/' ~/dotfiles/.vimrc
+rm -rf ~/dotfiles/.vimrc-e
 
 echo "---- terminal color setting ----";
 echo "Load terminal color profile in Terminal Setting";
