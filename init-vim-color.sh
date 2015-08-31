3a2fe2b56a1165f424081c915ee2d574e2a30335
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-08-31 16:41:03 +0900

    Fix install neobundle

--- init-vim-color.sh
+++ init-vim-color.sh
@@ -1,4 +1,3 @@
-sh -c "$(curl -fsSL https://raw.github.com/Shougo/neobundle.vim/master/bin/install.sh)";
 git clone https://github.com/tomislav/osx-terminal.app-colors-solarized ~/osx-terminal.app-colors-solarized
 LF=$'\\\x0A'
 sed -i -e 's/\" for replase: vim color setting/syntax enable'"$LF"'set background=dark'"$LF"'colorscheme solarized'"$LF"'let g:solarized_termcolors=256/' ~/dotfiles/.vimrc
