9286ba580705cae2512a60d5748d27755651e8a3
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2015-09-06 06:02:39 +0900

    Update install tmux

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/install/master/install)";
 
 brew doctor
-brew install zsh git tig caskroom/cask/brew-cask;
+brew install zsh git tig tmux caskroom/cask/brew-cask;
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
