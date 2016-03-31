c5b39acfcfdce96ec26e58655adb803132fd8bc8
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2016-03-31 15:49:29 +0900

    Fixed: direnv config

--- install_brew_list.sh
+++ install_brew_list.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+brew install `cat brew_list.txt`;
+
 echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zshenv;
 source ~/.zshenv;
-
-brew install `cat brew_list.txt`;
+echo "export EDITOR='vim'" >> ~/.zshrc;
+echo "eval '$(direnv hook zsh)'" >> ~/.zshrc;
+source ~/.zshrc;
