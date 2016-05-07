8226167af96499becca28d06e8fb86d7e48a6592
Kaoru Tsutsumishita <t.kaoru.kit@gmail.com>
2016-05-08 03:23:30 +0900

    Remove: link eslint

--- link.sh
+++ link.sh
@@ -3,13 +3,11 @@
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 source ~/.zshenv;
 source ~/.zshrc;
 source ~/.vimrc;
-source ~/.eslintrc;
 source ~/.tmux.conf;
 source ~/.gitconfig;
