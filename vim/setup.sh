0383a5022b7413004e19904d7c3261e9920c5c47
Kaoru Tsutsumishita <roana.enter@gmail.com>
2017-04-30 17:04:36 +0900

    Fix: dein setup

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # install dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh;
-sh ./installer.sh ~/.vim/bundle;
-rm -f ./installer.sh
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh;
+sh ~/installer.sh ~/.vim/bundle;
+rm -f ~/installer.sh;
